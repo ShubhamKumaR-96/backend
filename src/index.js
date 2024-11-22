27b2bcb4c20bde44deb5391058c33179676d9b1f
ShubhamKumaR-96 <shubhamkas245@gmail.com>
2024-11-22 19:16:23 +0530

    debugging and router

--- src/index.js
+++ src/index.js
@@ -1,17 +1,15 @@
 // require('dotenv').config({path:'./env'})
 
 import dotenv from "dotenv";
-
-import mongoose from "mongoose";
-import { DB_NAME } from "./constants.js";
+import { app } from "./app.js";
 import connectDB from "./db/index.js";
 
 dotenv.config({ path: "./env" });
 
 connectDB()
   .then(() => {
-    app.listem(process.env.PORT, () => {
-      `Server is running at ${process.env.PORT || 8000} `;
+    app.listen(process.env.PORT, () => {
+     console.log(`Server is running at http://localhost:${process.env.PORT || 8001}`)
     });
     app.on("err", (err) => {
       console.log("Error", err);
